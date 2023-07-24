3be6ae81b3761776018dc54f3f3cac282cec2d8a
Ildar Sharafutdinov <endlesssoap@gmail.com>
2023-07-24 20:23:12 +0400

    Product tiles should generate on load

--- src/lwc/productTile/productTile.js
+++ src/lwc/productTile/productTile.js
@@ -1,4 +1,28 @@
-import {LightningElement} from 'lwc';
+import {LightningElement, track} from 'lwc';
+import modalProduct from 'c/modalProduct'
+import getProducts from '@salesforce/apex/ProductController.getProducts';
 
 export default class ProductTile extends LightningElement {
+    @track pairs =[];
+    products =[];
+    connectedCallback(){
+        getProducts()
+            .then(result => {
+                this.products = result;
+                let pair = [];
+                console.log(pair);
+                for (let i = 0; i < this.products.length; i++)
+                {
+                    if ( i != 0 && i % 2 === 0) {
+                        this.pairs.push(pair);
+                        pair = [];
+                    }
+                    pair.push(this.products[i]);
+                }
+                if (pair.length > 0) this.pairs.push(pair);
+            });
+    }
+    openDetailsModal(event) {
+        let element = event.target;
+    }
 }
