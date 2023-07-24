7f0af337aa7e8902f7521eafd6f5111c9bdb1bd5
Ildar Sharafutdinov <endlesssoap@gmail.com>
2023-07-24 21:24:55 +0400

    Modal window should open properly now

--- src/lwc/productTile/productTile.js
+++ src/lwc/productTile/productTile.js
@@ -24,5 +24,22 @@ export default class ProductTile extends LightningElement {
     }
     openDetailsModal(event) {
         let element = event.target;
+        let tile = element.closest(".tile");
+
+        let tileImg = tile.querySelector(".slds-align_absolute-center").children[0].innerHTML;
+        let tileName = tile.querySelector(".tile-text-name").innerHTML;
+        let tileTypeFamily = tile.querySelector(".tile-text-types").innerHTML;
+        let tileDescription = tile.querySelector(".slds-line-clamp").innerHTML;
+        let tilePrice = tile.querySelector(".tile-text-bottom-price").innerHTML.toString();
+        let tileId = element.dataset.item;
+
+        const result = modalProduct.open({
+            img: tileImg,
+            name: tileName,
+            typeFamily: tileTypeFamily,
+            description: tileDescription,
+            price: tilePrice,
+            productId: tileId,
+        });
     }
 }
