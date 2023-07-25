88c762d90ea16691231fd3f988156ddcec5995ee
Ildar Sharafutdinov <endlesssoap@gmail.com>
2023-07-26 01:04:43 +0400

    product tiles now react to search bar, added interaction with parent component via getters and setters

--- src/lwc/productTile/productTile.js
+++ src/lwc/productTile/productTile.js
@@ -1,27 +1,52 @@
-import {LightningElement, track} from 'lwc';
+import {LightningElement, track, api} from 'lwc';
 import modalProduct from 'c/modalProduct'
 import getProducts from '@salesforce/apex/ProductController.getProducts';
 
 export default class ProductTile extends LightningElement {
     @track pairs =[];
-    products =[];
-    connectedCallback(){
-        getProducts()
+    @track products =[];
+
+    @api
+    get searchquery() {
+        return this.search;
+    }
+    set searchquery(value) {
+        this.search = value;
+        this.sendQuery();
+    }
+    @api
+    get filtermap() {
+        return this.filter;
+    }
+    set filtermap(value) {
+        this.filter = value;
+        this.sendQuery();
+    }
+
+    search = '';
+    filter = new Map();
+
+    sendQuery() {
+        getProducts({ searchBarQuery: this.search, filters: this.filter})
             .then(result => {
                 this.products = result;
-                let pair = [];
-                console.log(pair);
-                for (let i = 0; i < this.products.length; i++)
-                {
-                    if ( i != 0 && i % 2 === 0) {
-                        this.pairs.push(pair);
-                        pair = [];
-                    }
-                    pair.push(this.products[i]);
-                }
-                if (pair.length > 0) this.pairs.push(pair);
+                this.turnProductsIntoPairs(this.products);
             });
     }
+
+    turnProductsIntoPairs(products) {
+        this.pairs = [];
+        let pair = [];
+        for (let i = 0; i < products.length; i++) {
+            if (i != 0 && i % 2 === 0) {
+                this.pairs.push(pair);
+                pair = [];
+            }
+            pair.push(products[i]);
+        }
+        if (pair.length > 0) this.pairs.push(pair);
+    }
+
     openDetailsModal(event) {
         let element = event.target;
         let tile = element.closest(".tile");
