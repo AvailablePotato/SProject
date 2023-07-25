018460e9ece34f1291e5d9aca8d73c5dd4a9f77d
Ildar Sharafutdinov <endlesssoap@gmail.com>
2023-07-26 01:05:11 +0400

    filter panel front end finished, functionality remains to be added

--- src/lwc/filterPanel/filterPanel.js
+++ src/lwc/filterPanel/filterPanel.js
@@ -6,16 +6,21 @@ export default class FilterPanel extends LightningElement {
 
     picklists;
     activeSections = [];
+    fieldApiNameMap = new Map();
+    selectedFilters = new Map();
 
     @wire(getPicklistValuesByRecordType, {
         objectApiName: PRODUCT_OBJECT,
         recordTypeId: '012000000000000AAA',
     })
-
     picklistValues({data, error}) {
         if (data) {
             this.picklists = this.readPicklistValues(data.picklistFieldValues);
             this.activeSections = this.picklists.map((item) => item.label);
+            Object.keys(data.picklistFieldValues).forEach((picklist) => {
+                this.fieldApiNameMap.set(picklist.indexOf("__c") === -1 ? picklist : picklist.substring(0, picklist.indexOf("__c"))
+                    , picklist);
+            });
         }
         if (error) {
             console.log(error);
@@ -29,10 +34,26 @@ export default class FilterPanel extends LightningElement {
                 label: picklist.indexOf("__c") === -1 ? picklist : picklist.substring(0, picklist.indexOf("__c")),
                 items: picklistValues[picklist].values.map((item) => ({
                     label: item.label,
-                    name: item.value
+                    value: item.value,
                 }))
             });
         });
         return map;
+    }
+
+    handleCheckboxChange(event) {
+        let picklistName = event.target.name;
+        let newValues = event.detail.value;
+        let apiKey = this.fieldApiNameMap.get(picklistName);
+        if (newValues.length > 0) {
+            this.selectedFilters.set(apiKey, newValues);
+        }
+        else {
+            this.selectedFilters.delete(apiKey);
+        }
+        let mapCopy = new Map(Array.from(this.selectedFilters));
+        const filterUpdateEvent = new CustomEvent('filterchanged', {
+            detail: mapCopy});
+        this.dispatchEvent(filterUpdateEvent);
     }
 }
