47fee70b69420266f6b25a140d39af23d70a4ef3
Ildar Sharafutdinov <endlesssoap@gmail.com>
2023-07-26 01:03:33 +0400

    search bar's complete

--- src/lwc/searchBar/searchBar.js
+++ src/lwc/searchBar/searchBar.js
@@ -1,4 +1,19 @@
 import {LightningElement} from 'lwc';
 
+const DELAY = 300;
+
 export default class SearchBar extends LightningElement {
+    searchKey = '';
+
+    handleKeyChange(event) {
+        // Debouncing this method: Do not update the reactive property as long as this function is
+        // being called within a delay of DELAY. This is to avoid a very large number of Apex method calls.
+        window.clearTimeout(this.delayTimeout);
+        const searchKey = event.target.value;
+        this.delayTimeout = setTimeout(() => {
+            this.searchKey = searchKey;
+            const searchUpdateEvent = new CustomEvent('searchquerychanged', {detail: searchKey});
+            this.dispatchEvent(searchUpdateEvent);
+        }, DELAY);
+    }
 }
